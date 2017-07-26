7f034e5d740bb8ee9a33ce4591ef488527b0bb24
jiayinan <630312647@qq.com>
2017-07-26 17:48:32 +0800

    Initial commit

---
+++ flask_study.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test/')
+def test_page():
+    return 'This is a test page!'
+
+
+@app.route('/<name>/')
+def name_page(name):
+    return 'Hello, {name}!'.format(name=name)
+
+
+if __name__ == '__main__':
+    app.run()
